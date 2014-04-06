b5252ba883c4705b35200ac0b4d13660fc7f8570
WesleyG <wes.gillis@gmail.com>
2014-04-06 11:06:52 -0700

    up to 5.4

---
+++ spec/support/utilities.rb
@@ -0,0 +1,10 @@
+# files here are automatically included by rspec
+
+def full_title(page_title)
+  base_title = "Ruby on Rails Tutorial Sample App"
+  if page_title.empty?
+    base_title
+  else
+    "#{base_title} | #{page_title}"
+  end
+end
