0dd291fbffc655ae4e117d04631749a8de26eeb5
WesleyG <wes.gillis@gmail.com>
2014-04-06 12:27:19 -0700

    all tests passing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,6 +6,26 @@ describe "Static pages" do
   # called by 'it' in the tests
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end  
+
+  it "should have the right links on the layout" do
+  visit root_path
+  click_link "About"
+  expect(page).to have_title(full_title('About Us'))
+  click_link "Help"
+  expect(page).to have_title(full_title('Help'))
+  click_link "Contact"
+  expect(page).to have_title(full_title('Contact'))
+  click_link "Home"
+  click_link "Sign up now!"
+  expect(page).to have_title(full_title('Sign up'))
+  click_link "sample app"
+  expect(page).to have_content('Welcome to the Sample App')
+end
+
   describe "Home page" do
 
     # visit the root path before each example. 
@@ -13,18 +33,20 @@ describe "Static pages" do
     # which is a synonym.)
     before { visit root_path }
 
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
-    # full_title is called from spec/support/utilities.rb
+    let(:heading)    { 'Sample App' }
+    let(:page_title) { '' }
 
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
     before { visit help_path }
 
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
+    let(:heading)    { 'Help' }
+    let(:page_title) { 'Help' }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
@@ -37,7 +59,9 @@ describe "Static pages" do
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_content('Contact') }
+    it { should have_selector('h1', text: 'Contact') }
+    # have_selector is from Capybara and allows to test for 
+    # specific tags such as h1
     it { should have_title(full_title('Contact')) }
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,13 +1,18 @@
 require 'spec_helper'
 
-describe "User pages" do
+describe ApplicationHelper do
 
-  subject { page }
+  describe "full_title" do
+    it "should include the page title" do
+      expect(full_title("foo")).to match(/foo/)
+    end
 
-  describe "signup page" do
-    before { visit signup_path }
+    it "should include the base title" do
+      expect(full_title("foo")).to match(/^Ruby on Rails Tutorial Sample App/)
+    end
 
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
+    it "should not include a bar for the home page" do
+      expect(full_title("")).not_to match(/\|/)
+    end
   end
 end
