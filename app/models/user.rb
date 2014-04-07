da3021898fa1731b2dffa537f71375a42af3edf5
WesleyG <wes.gillis@gmail.com>
2014-04-06 22:55:41 -0700

    completed chapter 6 but password authenticate not working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,17 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase }
+  has_secure_password
+  # has_secure_password does a lot, needs a password_digest column in
+  # db to work correctly
+  before_save { email.downcase! }  
   # the above prevents email duplication by downcasing first
   
   validates :name, presence: true, length: { maximum: 50 }
   # validates(:name, presence: true) is also valid
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, 
-		format: { with: VALID_EMAIL_REGEX }, 
-		uniqueness: { case_sensitive: false } 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
     # rails infers that uniquess: true from the above statement
-  has_secure_password
-  # has_secure_password does a lot, needs a password_digest column in
-  # db to work correctly
+
+
   validates :password, length: { minimum: 6 }
 end
