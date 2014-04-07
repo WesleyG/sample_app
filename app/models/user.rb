5fcb1d8db121f78e6e3f137fc47484c427089a64
WesleyG <wes.gillis@gmail.com>
2014-04-06 22:47:17 -0700

    up to 6.3.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,12 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   # validates(:name, presence: true) is also valid
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
-    uniqueness: { case_sensitive: false } 
+  validates :email, presence: true, 
+		format: { with: VALID_EMAIL_REGEX }, 
+		uniqueness: { case_sensitive: false } 
     # rails infers that uniquess: true from the above statement
+  has_secure_password
+  # has_secure_password does a lot, needs a password_digest column in
+  # db to work correctly
+  validates :password, length: { minimum: 6 }
 end
