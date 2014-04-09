978dce2163d6b8d13c3bed5753218bb67bdc19c1
WesleyG <wes.gillis@gmail.com>
2014-04-08 21:53:47 -0700

    all tests passing up to 7.4.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,22 @@ class UsersController < ApplicationController
   def new
   	@user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # handle a successful save by redirecting to user page
+      # combination of redirect and @user elimates need for user_url
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
