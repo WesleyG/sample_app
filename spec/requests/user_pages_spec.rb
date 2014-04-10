18be0c2d052a620b14f1a83576d935c86ce8f30f
WesleyG <wes.gillis@gmail.com>
2014-04-09 22:28:00 -0700

    up to chapter 8

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -30,6 +30,21 @@ describe "User pages" do
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
+
+        describe "after submission" do
+          before { click_button submit }
+
+          it { should have_title('Sign up') }
+          it { should have_content('error') }
+        end
+
+        describe "after saving the user" do
+          before { click_button submit }
+          let(:user) { User.find_by(email: 'user@example.com') }
+
+          it { should have_title(user.name) }
+          it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        end
       end
 
       it "should create a user" do
