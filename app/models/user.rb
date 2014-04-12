88fa6eb8467ba6fe0ca74ab9e7d3996d12a8c96e
WesleyG <wes.gillis@gmail.com>
2014-04-11 20:01:00 -0700

    signin working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ActiveRecord::Base
-  has_secure_password
+
+  # the above prevents email duplication by downcasing first  
+  before_save { self.email = email.downcase }
+  before_create :create_remember_token
+
   # has_secure_password does a lot, needs a password_digest column in
   # db to work correctly
-  before_save { email.downcase! }  
-  # the above prevents email duplication by downcasing first
-  
-  validates :name, presence: true, length: { maximum: 50 }
+  has_secure_password
+
   # validates(:name, presence: true) is also valid
+  validates :name, presence: true, length: { maximum: 50 }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
@@ -14,4 +18,18 @@ class User < ActiveRecord::Base
 
 
   validates :password, length: { minimum: 6 }
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.hash(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.hash(User.new_remember_token)
+    end
 end
